03f3f483d23633ae37ee18ff380eeceacb083da7
adam <adammarey51@gmail.com>
2023-06-30 21:52:04 -0700

    product ts types - price foramt helper function

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
-export default function Product({ name, image, price }) {
+import formatPrice from '@/util/PriceFormat';
+import { ProductType } from '@/types/ProductType';
+export default function Product({ name, image, price }: ProductType) {
   return (
     <div>
       <Image src={image} alt={name} width={400} height={400} />
       <h1>
         {name}
       </h1>
-      <h2>
-        {price}
-      </h2>
+      {price !== null ? formatPrice(price) : 'N/A'}
     </div>
   );
 }
---
+++ util/PriceFormat.ts
@@ -0,0 +1,7 @@
+const formatPrice = (amount: number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(amount / 100);
+};
+export default formatPrice;
