cc3d2d804dfa4843cbf07eb2bb033e4026e83bb7
Harrison Lynch <harrisonlynch@Harrisons-MBP.lan>
2023-07-03 19:25:55 -0700

    added zustand/state management, cart icon in nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,15 +4,25 @@ import { Session } from 'next-auth';
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import Cart from './Cart';
+import { useCartStore } from '@/store';
+import {AiFillShopping} from 'react-icons/ai'
 
 export default function Nav({ user }: Session) {
+  const cartStore = useCartStore()
   return (
-    <nav className="flex justify-between items-center py-8">
+    <nav className="flex justify-between items-center py-12">
       <Link href={'/'}>
         <h1>Attire Alchemy</h1>
       </Link>
       <ul className="flex items-center gap-12">
         {/* is the user is not signed in */}
+        <li className='flex items-center text-3xl relative cursor-pointer'>
+          <AiFillShopping />
+          <span className='bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center'>
+            {cartStore.cart.length}
+          </span>
+        </li>
         {!user &&
           <li className="bg-teal-600 test-white py-2 px-4 rounded-md mb-4">
             <button onClick={() => signIn()}>Sign In</button>
@@ -30,6 +40,7 @@ export default function Nav({ user }: Session) {
             </li>
           </>}
       </ul>
+      {cartStore.isOpen && <Cart />}
     </nav>
   );
 }
