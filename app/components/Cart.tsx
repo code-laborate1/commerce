88b815df817917d4db4a36e94e38921c116285a4
LynchHarrison <hblynch2001@gmail.com>
2023-07-07 11:50:07 -0700

    total price added to cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -8,6 +8,11 @@ export default function Cart() {
   const cartStore = useCartStore();
   console.log(cartStore.isOpen);
 
+  //total price
+  const totalPrice = cartStore.cart.reduce((acc, item) => {
+    return acc + item.unit_amount * item.quantity
+  }, 0)
+
   return (
     <div
       onClick={() => cartStore.toggleCart()}
@@ -67,6 +72,8 @@ export default function Cart() {
             </div>
           </div>
         )}
+        {/* Checkout and total */}
+        <p>Total: {formatPrice(totalPrice)}</p>
         {cartStore.cart.length > 0 &&
           <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
             Checkout
