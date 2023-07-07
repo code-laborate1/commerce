7b7d8fd1ffbf997f656c0f3033454a0c55a62bd2
LynchHarrison <hblynch2001@gmail.com>
2023-07-07 12:29:21 -0700

    added animations to cart icon and cart page on removal of 1 item and all items

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -4,9 +4,10 @@ import { useCartStore } from '@/store';
 import formatPrice from '@/util/PriceFormat';
 import { IoAddCircle, IoRemoveCircle } from 'react-icons/io5';
 import basket from '@/public/cart-empty.png';
+import { motion, AnimatePresence } from 'framer-motion'
+
 export default function Cart() {
   const cartStore = useCartStore();
-  console.log(cartStore.isOpen);
 
   //total price
   const totalPrice = cartStore.cart.reduce((acc, item) => {
@@ -14,17 +15,21 @@ export default function Cart() {
   }, 0)
 
   return (
-    <div
+    <motion.div
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
       onClick={() => cartStore.toggleCart()}
       className="fixed w-full h-screen left-0 top-0 bg-black/25"
     >
-      <div
+      <motion.div
+        layout
         onClick={e => e.stopPropagation()}
         className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700"
       >
         <h1>Here's your shopping list </h1>
         {cartStore.cart.map(item =>
-          <div className="flex py-4 gap-4">
+          <motion.div layout key={item.id} className="flex py-4 gap-4">
             <Image
               className="rounded-md h-25"
               src={item.image}
@@ -70,26 +75,35 @@ export default function Cart() {
                 {item.unit_amount && formatPrice(item.unit_amount)}
               </p>
             </div>
-          </div>
+          </motion.div>
         )}
         {/* Checkout and total */}
-        <p>Total: {formatPrice(totalPrice)}</p>
-        {cartStore.cart.length > 0 &&
-          <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
-            Checkout
-          </button>}
-        {cartStore.cart.length === 0 &&
-          <div>
-            <Image
-              className="flex flex-col items-center gap-12 text-2xl font-medium pt-56 opacity-75"
-              src={basket}
-              alt="basket"
-              width={400}
-              height={400}
-            />
-            <h2 className="text-center text-2xl">Your cart is empty ! </h2>
-          </div>}
-      </div>
-    </div>
+          {cartStore.cart.length > 0 &&
+           <motion.div layout>
+            <p>Total: {formatPrice(totalPrice)}</p>
+            <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
+              Checkout
+            </button>
+            </motion.div>
+            }
+        <AnimatePresence>
+          {cartStore.cart.length === 0 &&
+            <motion.div
+            animate={{scale:1, rotateZ: 0, opacity: 0.75}}
+            initial={{scale: 0.5, rotateZ: -10, opacity: 0}}
+            exit={{scale: 0.5, rotateZ: -10, opacity: 0}}
+            >
+              <Image
+                className="flex flex-col items-center gap-12 text-2xl font-medium pt-56 opacity-75"
+                src={basket}
+                alt="basket"
+                width={400}
+                height={400}
+              />
+              <h2 className="text-center text-2xl">Your cart is empty ! </h2>
+            </motion.div>}
+          </AnimatePresence>
+      </motion.div>
+    </motion.div>
   );
 }
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import Cart from './Cart';
 import { useCartStore } from '@/store';
 import {HiOutlineShoppingCart} from 'react-icons/hi'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function Nav({ user }: Session) {
   const cartStore = useCartStore()
@@ -20,9 +21,17 @@ export default function Nav({ user }: Session) {
         <li onClick={() => cartStore.toggleCart()}
         className='flex items-center text-3xl relative cursor-pointer'>
           <HiOutlineShoppingCart />
-          <span className='bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-5 bottom-4 flex items-center justify-center'>
-            {cartStore.cart.length}
-          </span>
+          <AnimatePresence>
+            {cartStore.cart.length > 0 && (
+              <motion.span 
+              animate={{scale: 1}} 
+              initial={{scale: 0}}
+              exit={{scale: 0}}
+              className='bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-5 bottom-4 flex items-center justify-center'>
+                {cartStore.cart.length}
+              </motion.span>
+            )}
+          </AnimatePresence>
         </li>
 
         {/* is the user is not signed in */}
@@ -43,7 +52,7 @@ export default function Nav({ user }: Session) {
             </li>
           </>}
       </ul>
-      {cartStore.isOpen && <Cart />}
+      <AnimatePresence>{cartStore.isOpen && <Cart />}</AnimatePresence>
     </nav>
   );
 }
