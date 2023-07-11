e8506c2c446d8f3c881040013f1de081604ac926
adam <adammarey51@gmail.com>
2023-07-11 08:46:15 -0700

    create payment intent - saving orders

--- app/components/Checkout.tsx
+++ app/components/Checkout.tsx
@@ -1,41 +1,46 @@
-'use client'
+'use client';
 
-import { loadStripe, StripeElementsOptions } from '@stripe/stripe-js'
-import { Elements } from '@stripe/react-stripe-js'
-import { useCartStore } from '@/store'
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { loadStripe, StripeElementsOptions } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { useCartStore } from '@/store';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
 
 export default function Checkout() {
-  const cartStore = useCartStore()
-  const router = useRouter()
-  const [clientSecret, setClientSecret] = useState('')
+  const cartStore = useCartStore();
+  const router = useRouter();
+  const [clientSecret, setClientSecret] = useState('');
 
   useEffect(() => {
     //create a paymentIntent as soon as the component loads
     fetch('/api/create-payment-intent', {
-     method: 'POST',
-      headers: {'Content-Type': 'application/json'},
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         items: cartStore.cart,
-        payment_intent_id: cartStore.paymentIntent,
+        payment_intent_id: cartStore.paymentIntent
       })
-    }).then((res) => {
-      if (res.status === 403) {
-        return router.push('/api/auth/signin')
-      }
-      return res.json()
-      // SET CLIENT SECRET and the paymentIntent associated with it
-    }).then((data) => {
-      console.log(data)
     })
-  },[])
+      .then(res => {
+        if (res.status === 403) {
+          return router.push('/api/auth/signin');
+        }
+        return res.json();
+        // SET CLIENT SECRET and the paymentIntent associated with it
+      })
+      .then(data => {
+        setClientSecret(data.paymentIntent.client_secret);
+        cartStore.setPaymentIntent(data.paymentIntent.id);
+      });
+  }, []);
 
-    return (
-      <div>
-        <h1>Checkout</h1>
-      </div>
-    )
+  return (
+    <div>
+      <h1>Checkout</h1>
+    </div>
+  );
 }
