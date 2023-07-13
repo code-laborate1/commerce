128c45acebb85cf59111c74dd2b4ff782ca952cf
adam <adammarey51@gmail.com>
2023-07-13 13:23:39 -0700

    fetched order to dashboard - dashboard styling

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,11 +6,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Cart from './Cart';
 import { useCartStore } from '@/store';
-import {HiOutlineShoppingCart} from 'react-icons/hi'
-import { motion, AnimatePresence } from 'framer-motion'
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Nav({ user }: Session) {
-  const cartStore = useCartStore()
+  const cartStore = useCartStore();
   return (
     <nav className="flex justify-between items-center py-12">
       <Link href={'/'}>
@@ -18,19 +18,21 @@ export default function Nav({ user }: Session) {
       </Link>
       <ul className="flex items-center gap-12">
         {/* toggle the cart */}
-        <li onClick={() => cartStore.toggleCart()}
-        className='flex items-center text-3xl relative cursor-pointer'>
+        <li
+          onClick={() => cartStore.toggleCart()}
+          className="flex items-center text-3xl relative cursor-pointer"
+        >
           <HiOutlineShoppingCart />
           <AnimatePresence>
-            {cartStore.cart.length > 0 && (
-              <motion.span 
-              animate={{scale: 1}} 
-              initial={{scale: 0}}
-              exit={{scale: 0}}
-              className='bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-5 bottom-4 flex items-center justify-center'>
+            {cartStore.cart.length > 0 &&
+              <motion.span
+                animate={{ scale: 1 }}
+                initial={{ scale: 0 }}
+                exit={{ scale: 0 }}
+                className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-5 bottom-4 flex items-center justify-center"
+              >
                 {cartStore.cart.length}
-              </motion.span>
-            )}
+              </motion.span>}
           </AnimatePresence>
         </li>
 
@@ -40,7 +42,7 @@ export default function Nav({ user }: Session) {
             <button onClick={() => signIn()}>Sign In</button>
           </li>}
         {user &&
-          <>
+          <Link href={'/dashboard'}>
             <li>
               <Image
                 src={user.image as string}
@@ -50,9 +52,11 @@ export default function Nav({ user }: Session) {
                 className="rounded-full mb-4"
               />
             </li>
-          </>}
+          </Link>}
       </ul>
-      <AnimatePresence>{cartStore.isOpen && <Cart />}</AnimatePresence>
+      <AnimatePresence>
+        {cartStore.isOpen && <Cart />}
+      </AnimatePresence>
     </nav>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,73 @@
 import React from 'react';
+import { PrismaClient } from '@prisma/client';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import formatPrice from '@/util/PriceFormat';
+import Image from 'next/image';
 
-const page = () => {
+export const revalidate = 0;
+const fetchOrders = async () => {
+  const prisma = new PrismaClient();
+  const user = await getServerSession(authOptions);
+  if (!user) {
+    return null;
+  }
+  const orders = await prisma.order.findMany({
+    where: {
+      userId: user?.user?.id, status: 'complete'
+    },
+    include: {
+      products: true,
+    },
+  });
+  return orders;
+};
+
+export default async function Dashboard() {
+  const orders = await fetchOrders();
+
+  if (orders === null) {
+    return <h1 className="text-4xl font-semibold text-center text-red-600">Not authenticated - Please Log in to view your orders.</h1>;
+  }
+
+  if (orders.length === 0) {
+    return <h1 className="text-4xl font-semibold text-center text-blue-600">No Orders placed.</h1>;
+  }
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {orders.map((order) => (
+          <div className="p-4 bg-white rounded-xl shadow-lg dark:bg-gray-800" key={order.id}>
+            <h2 className="text-lg font-semibold text-gray-700 dark:text-white">Order reference: {order.id}</h2>
+            <p className="text-xs mt-2 text-gray-500 dark:text-gray-400">Time: {new Date(order.createdAt).toString()}</p>
+            <p className="text-xs mt-2 text-gray-500 dark:text-gray-400">
+              Status: <span className={`font-bold text-xs ${order.status === 'complete' ? 'text-green-500' : 'text-orange-500'}`}>{order.status}</span>
+            </p>
+            <p className="text-md mt-2 font-semibold text-gray-700 dark:text-white">Total: {formatPrice(order.amount)}</p>
+            
+            <div className="grid grid-cols-1 gap-4 mt-4">
+              {
+                order.products.map((product) => (
+                  <div className="flex space-x-4 items-center" key={product.id}>
+                    <Image
+                      src={product.image!}
+                      alt={product.name}
+                      width={40}
+                      height={40}
+                    />
+                    <div>
+                      <h2 className="text-sm font-semibold text-gray-700 dark:text-white">{product.name}</h2>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">Price: {formatPrice(product.unit_amount)}</p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">Quantity: {product.quantity}</p>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
-export default page;
